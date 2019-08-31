fb095b1317fe566d1710a911140f6cf2ee733902
Uzair Ali <uzaira960@gmail.com>
2019-08-31 17:37:04 +0100

    Added call for simulate_round()

--- simulating_aggression.py
+++ simulating_aggression.py
@@ -1,4 +1,4 @@
-from sim import organisms as org, utils
+from sim import organisms as org, simulation
 
 def main():
     # num_of_organisms = input("Number of organisms: ")
@@ -7,12 +7,20 @@ def main():
     num_of_doves = 5
     # num_of_hawks = input("Number of Hawks: ")
     num_of_hawks = 5
+    # num_of_hawks = input("Number of Hawks: ")
+    num_of_rounds = 5
     assert num_of_doves + num_of_hawks == num_of_organisms, "Check the number of organisms!"
 
     organisms = [org.Dove()] * num_of_doves + [org.Hawk()] * num_of_hawks
 
-    print(len(organisms))
     # print(type(organisms[0]) == org.Dove)
+    for i in range(num_of_rounds):
+        organisms = simulation.simulate_round(organisms)
 
 if __name__ == '__main__':
     main()
+
+# TODO: 1. Function to assign pairs
+#       2. Dictionary that holds values of strategies
+#       3. Function to evaluates values of strategies
+#       4. Dictionary that holds tuple values
