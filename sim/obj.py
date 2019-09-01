5041a43b82628287cfd1a7316ba7517127f2df4f
Uzair Ali <uzaira960@gmail.com>
2019-09-01 18:47:30 +0100

    Added Food object

---
+++ sim/obj.py
@@ -0,0 +1,42 @@
+class Food:
+
+    def __init__(self):
+        self.consumers = []
+
+    def add_consumer(organism):
+        self.consumers.append(organism)
+
+    def get_consumers():
+        return self.consumers
+
+    def num_consumers():
+        return len(self.consumers)
+
+
+class Organism:
+
+    def __init__(self, chance_of_survival=0, chance_of_reproduction=0):
+        # float 0-1
+        self.chance_of_survival = chance_of_survival
+        # float 0-1
+        self.chance_of_reproduction = chance_of_reproduction
+
+    def set_chance_of_survival(self, chance):
+        self.chance_of_survival = chance
+
+    def get_chance_of_survival(self):
+        return self.chance_of_survival
+
+    def set_chance_of_reproduction(self, chance):
+        self.chance_of_reproduction = chance
+
+    def get_chance_of_reproduction(self):
+        return self.chance_of_reproduction
+
+
+class Dove(Organism):
+    pass
+
+
+class Hawk(Organism):
+    pass
