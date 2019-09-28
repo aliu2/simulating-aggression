3ec630d37c4e2b9d28fe380116539c80224330dd
Uzair Ali <uzaira960@gmail.com>
2019-09-28 14:12:12 +0100

    Finding food and calculating strategy scores added

--- sim/simulation.py
+++ sim/simulation.py
@@ -1,31 +1,61 @@
 import random
 import obj
+import chance
 
 
 def simulate_round(num_of_doves, num_of_hawks):
-    organisms = [obj.Organism("Dove")] * num_of_doves + [obj.Organism("Hawk")] * num_of_hawks
-    food = [obj.Food()] * (num_of_doves + num_of_hawks)
-
+    doves = [obj.Organism("Dove") for i in range(num_of_doves)]
+    hawks = [obj.Organism("Hawk") for i in range(num_of_hawks)]
+    organisms = doves + hawks
+    food = [obj.Food() for i in range(3)]
     fed_organisms = find_food(organisms, food)
 
-# TODO: 1. Probability of finding food is incorrect
+
 def find_food(organisms, untaken_food):
-    # list of tuples
     taken_food = []
 
     while len(organisms) > 0:
         chosen_food = random.randint(0, len(untaken_food)-1)
-        untaken_food[chosen_food].add_consumer(organisms.pop(0))
+        curr_organism = organisms.pop(0)
+        untaken_food[chosen_food].add_consumer(curr_organism)
         if untaken_food[chosen_food].num_consumers() == 2:
             taken_food.append(untaken_food.pop(chosen_food))
-    print(untaken_food)
-    for org in untaken_food:
-        print(org)
+
+    return taken_food + untaken_food
+
+
+def filter_food(list_of_food):
+    taken_food = []
+    for food in list_of_food:
+        if food.num_consumers() > 0:
+            taken_food.append(food)
+
     return taken_food
 
 
-def evaluate_chances(organisms):
-    pass
+def calc_strategy_scores(taken_food):
+    strategy_scores = {
+        "Dove": 1,
+        "Hawk": 3
+    }
+
+    organism_scores = []
+    for food in taken_food:
+        score = 0
+        for organism in food.get_consumers():
+            score += strategy_scores[organism.get_strategy()]
+        organism_scores.append(score)
+
+    return organism_scores
+
+
+def evaluate_chances(list_of_chances):
+    result = []
+
+    for i in list_of_chances:
+        result.append(chance.switch(i))
+
+    return result
 
 
 def evolutionize(organisms):
@@ -33,11 +63,17 @@ def evolutionize(organisms):
 
 
 def main():
-    num_of_doves = 3
-    num_of_hawks = 3
-    organisms = [obj.Organism("Dove")] * num_of_doves + [obj.Organism("Hawk")] * num_of_hawks
-    food = [obj.Food()] * 3
-    find_food(organisms, food)
+    # num_of_doves = 3
+    # num_of_hawks = 3
+    # doves = [obj.Organism("Dove") for i in range(num_of_doves)]
+    # hawks = [obj.Organism("Hawk") for i in range(num_of_hawks)]
+    # organisms = doves + hawks
+    # food = [obj.Food() for i in range(10)]
+    # food = find_food(organisms, food)
+    # food = filter_food(food)
+    # strategy_scores = calc_strategy_scores(food)
+    print(evaluate_chances([1, 1, 3, 4]))
+    pass
 
 
 if __name__ == '__main__':
